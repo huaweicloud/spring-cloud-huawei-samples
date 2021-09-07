7d881b8109748a4211864856d4c7e667b83e4cf5
liubao68 <bismy@qq.com>
2021-09-07 20:21:45 +0800

    improve feign client declaration

--- hsf-pandora-boot/hsf-pandora-boot-consumer/src/main/java/com/alibaba/edas/HSFConsumerApplication.java
+++ hsf-pandora-boot/hsf-pandora-boot-consumer/src/main/java/com/alibaba/edas/HSFConsumerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(clients= {HelloServiceFeignClient.class})
+@EnableFeignClients
 public class HSFConsumerApplication {
 
     public static void main(String[] args) {
