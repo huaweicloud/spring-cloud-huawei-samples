78ba8d47134719b2781116094e29318a5b416d73
david6969xin <1045319818@qq.com>
2021-11-22 20:53:43 +0800

    add dubbo、safa migrate demo

---
+++ dubbo-safa-demo/dubbo-demo/common-api/src/main/java/com/huaweicloud/sample/api/HelloService.java
@@ -0,0 +1,5 @@
+package com.huaweicloud.sample.api;
+
+public interface HelloService {
+    String hello(String name);
+}
---
+++ dubbo-safa-demo/sofastack-demo/sofastack-provider/src/main/java/com/huaweicloud/sample/SafaProviderApplication.java
@@ -0,0 +1,11 @@
+package com.huaweicloud.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafaProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SafaProviderApplication.class,args);
+    }
+}
