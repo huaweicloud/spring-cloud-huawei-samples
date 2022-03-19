080ac96856f78369b98fba8df2556ae8360cdad8
liubao <bismy@qq.com>
2022-03-19 17:11:07 +0800

    使用工具替换的结果

--- migrate/hsf/hsf-pandora-boot-springcloud/hsf-pandora-boot-consumer/src/main/java/com/alibaba/edas/HSFConsumerApplication.java
+++ migrate/hsf/hsf-pandora-boot-springcloud/hsf-pandora-boot-consumer/src/main/java/com/alibaba/edas/HSFConsumerApplication.java
@@ -1,13 +1,14 @@
 package com.alibaba.edas;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @EnableDiscoveryClient
 @EnableFeignClients
+@SpringBootApplication
 public class HSFConsumerApplication {
 
     public static void main(String[] args) {
--- migrate/hsf/hsf-pandora-boot-springcloud/hsf-pandora-boot-provider/src/main/java/com/alibaba/edas/HSFProviderApplication.java
+++ migrate/hsf/hsf-pandora-boot-springcloud/hsf-pandora-boot-provider/src/main/java/com/alibaba/edas/HSFProviderApplication.java
@@ -1,11 +1,14 @@
 package com.alibaba.edas;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
 public class HSFProviderApplication {
 
     public static void main(String[] args) {
