97b7bb7f5880b2b35dee4b5bae762a23b7907752
david6969xin <86217945+david6969xin@users.noreply.github.com>
2022-03-25 18:00:16 +0800

    修改eureka demo并移动到migrate文件中 (#61)

--- migrate/eureka/eureka-demo/eureka-consumer/src/main/java/com/springcloud/eureka/samples/ConsumerApplication.java
+++ migrate/eureka/eureka-demo/eureka-consumer/src/main/java/com/springcloud/eureka/samples/ConsumerApplication.java
@@ -3,12 +3,12 @@ package com.springcloud.eureka.samples;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 public class ConsumerApplication {
   public static void main(String[] args) {
     SpringApplication.run(ConsumerApplication.class, args);
