55de4a2fb33f232f7d761e41ce4326eca7072c29
david6969xin <1045319818@qq.com>
2022-03-25 16:25:33 +0800

    修改eureka demo并移动到migrate文件中

--- migrate/eureka/eureka-demo/eureka-consumer/src/main/java/com/springcloud/eureka/samples/ConsumerController.java
+++ migrate/eureka/eureka-demo/eureka-consumer/src/main/java/com/springcloud/eureka/samples/ConsumerController.java
@@ -1,14 +1,19 @@
 package com.springcloud.eureka.samples;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class ProviderController {
-  // a very simple service to echo the request parameter
+public class ConsumerController {
+
+  @Autowired
+  private RestTemplate restTemplate;
+
   @GetMapping("/sayHello")
   public String sayHello(@RequestParam("name") String name) {
-    return "Hello " + name;
+    return restTemplate.getForObject("http://eureka-provider/sayHello?name={1}", String.class, name);
   }
 }
