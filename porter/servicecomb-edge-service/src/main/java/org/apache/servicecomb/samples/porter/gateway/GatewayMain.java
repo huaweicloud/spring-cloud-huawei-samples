c1f6c52948d7e2b97fb193e69da7669a252f1426
liubao <bismy@qq.com>
2022-07-17 23:33:11 +0800

    [porter-refactor][part1: remove servicecomb-edge-service]

--- porter/servicecomb-edge-service/src/main/java/org/apache/servicecomb/samples/porter/gateway/GatewayMain.java
+++
@@ -1,9 +0,0 @@
-package org.apache.servicecomb.samples.porter.gateway;
-
-import org.apache.servicecomb.foundation.common.utils.BeanUtils;
-
-public class GatewayMain {
-    public static void main(String[] args) throws Exception {
-        BeanUtils.init();
-    }
-}
