a805701662792549e1c3b38042e43a11dca5f9a8
liubao <bismy@qq.com>
2022-07-18 21:21:12 +0800

    [porter-refactor][part7: add authentication]

--- porter/file-business/src/main/java/com/huaweicloud/samples/porter/file/controler/FileController.java
+++ porter/file-business/src/main/java/com/huaweicloud/samples/porter/file/controler/FileController.java
@@ -1,5 +1,8 @@
 package com.huaweicloud.samples.porter.file.controler;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.servicecomb.foundation.common.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.huaweicloud.common.context.InvocationContextHolder;
 import com.huaweicloud.samples.porter.file.api.FileService;
+import com.huaweicloud.samples.porter.file.service.SessionInfo;
 
 @RestController
 @RequestMapping(path = "/v1/file")
@@ -28,7 +33,25 @@ public class FileController {
   }
 
   @DeleteMapping(path = "/delete", produces = MediaType.APPLICATION_JSON_VALUE)
-  public boolean deleteFile(@RequestParam(name = "id") String id) {
+  public boolean deleteFile(@RequestParam(name = "id") String id, HttpServletResponse response) {
+    String session = InvocationContextHolder.getOrCreateInvocationContext().getContext("session-info");
+    if (session == null) {
+      response.setStatus(503);
+      return false;
+    } else {
+      SessionInfo sessionInfo = null;
+      try {
+        sessionInfo = JsonUtils.readValue(session.getBytes("UTF-8"), SessionInfo.class);
+      } catch (Exception e) {
+        response.setStatus(503);
+        return false;
+      }
+      if (sessionInfo == null || !sessionInfo.getRoleName().equals("admin")) {
+        response.setStatus(503);
+        return false;
+      }
+    }
+
     return fileService.deleteFile(id);
   }
 }
