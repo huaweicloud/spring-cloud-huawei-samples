249d7a560ea8815f68a70abe44a1d411deae69ce
liubao <bismy@qq.com>
2022-07-18 13:31:50 +0800

    [porter-refactor][part5: make ui application run]

--- porter/user-core/src/main/java/com/huaweicloud/samples/porter/user/api/UserEndpoint.java
+++ porter/user-core/src/main/java/com/huaweicloud/samples/porter/user/api/UserEndpoint.java
@@ -10,25 +10,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/v1/user", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserEndpoint {
   @Autowired
   private UserService userService;
 
-  @PostMapping(path = "/v1/user/login", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(path = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
   public SessionInfo login(@RequestParam(name = "userName") String userName,
       @RequestParam(name = "password") String password) {
     return userService.login(userName, password);
   }
 
-  @GetMapping(path = "/v1/user/session", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(path = "/session", produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
   public SessionInfo getSession(@RequestParam(name = "sessionId") String sessionId) {
     return userService.getSession(sessionId);
   }
 
-  @GetMapping(path = "/v1/user/ping", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(path = "/ping", produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
   public String ping(@RequestParam(name = "message") String message) {
     return userService.ping(message);
