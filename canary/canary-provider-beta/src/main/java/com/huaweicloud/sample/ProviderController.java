4c53b64e4b558644b2a092cbbc4958ab178472c3
chengyouling <spadgerlin888@163.com>
2023-12-22 11:56:13 +0800

    [#116] upgrade springboot/cloud version and add fallback/circuit test demo

--- canary/canary-provider-beta/src/main/java/com/huaweicloud/sample/ProviderController.java
+++ canary/canary-provider-beta/src/main/java/com/huaweicloud/sample/ProviderController.java
@@ -1,5 +1,7 @@
 package com.huaweicloud.sample;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +17,28 @@ public class ProviderController {
   @Value("${server.port}")
   private Integer port;
 
+  @Value("${spring.cloud.servicecomb.discovery.version}")
+  private String version;
+
   @RequestMapping("/provider")
   public String sayHello(@RequestParam("id") String id) {
-    return "beta: provider ---> " + id + " port -->" + port;
+    return "beta: provider ---> " + id + " port -->" + port + " ,version--->" + version;
+  }
+
+  @RequestMapping("/canaryFallback")
+  public String canaryFallback() {
+    return "beta: provider test fallback method, port -->" + port + " ,version--->" + version;
+  }
+
+  @RequestMapping("/canaryHeaderIsolation")
+  public String canaryHeaderIsolation(HttpServletResponse response) {
+    response.addHeader("X-HTTP-STATUS-CODE", "502");
+    return "beta: provider isolation method, port -->" + port + " ,version--->" + version;
+  }
+
+  @RequestMapping("/canaryHeaderCircuit")
+  public String canaryHeaderCircuit(HttpServletResponse response) {
+    response.addHeader("X-HTTP-STATUS-CODE", "502");
+    return "beta: provider circuit method, port -->" + port + " ,version--->" + version;
   }
 }
--- canary/canary-provider/src/main/java/com/huaweicloud/sample/ProviderApplication.java
+++ canary/canary-provider/src/main/java/com/huaweicloud/sample/ProviderApplication.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Application {
+public class ProviderApplication {
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(ProviderApplication.class, args);
   }
 }
