edbcf05db7599f6b0cbe8d899a623724c7eca277
Cheng YouLing <97039406+chengyouling@users.noreply.github.com>
2023-12-26 17:21:41 +0800

    [#116] upgrade springboot/cloud version and add fallback/circuit test demo (#117)

--- canary/canary-consumer/src/main/java/com/huaweicloud/sample/ConsumerController.java
+++ canary/canary-consumer/src/main/java/com/huaweicloud/sample/ConsumerController.java
@@ -1,6 +1,9 @@
 package com.huaweicloud.sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,37 @@ public class ConsumerController {
           ConsumerController.d++;
         }).get();
     return result + "   " + d;
+  }
+
+  @RequestMapping("/canaryFallback")
+  public String canaryFallback() {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("canary", "fallback");
+    HttpEntity<Void> entity = new HttpEntity<>(headers);
+    return restTemplate.exchange("http://canary-provider/canaryFallback", HttpMethod.GET, entity,
+        String.class).getBody();
+  }
+
+  @RequestMapping("/canaryHeaderIsolation")
+  public String canaryHeaderIsolation() {
+    try {
+      return restTemplate.getForObject("http://canary-provider/canaryHeaderIsolation", String.class);
+    } catch (Exception e) {
+      return "instance isolated";
+    }
+  }
+
+  @RequestMapping("/canaryHeaderCircuit")
+  public String canaryHeaderCircuit() {
+    try {
+      return restTemplate.getForObject("http://canary-provider/canaryHeaderCircuit", String.class);
+    } catch (Exception e) {
+      return "circuitBreaker open";
+    }
+  }
+
+  @RequestMapping("/canaryHeaderRetry")
+  public String canaryHeaderRetry() {
+    return restTemplate.getForObject("http://canary-provider/canaryHeaderRetry", String.class);
   }
 }
--- canary/canary-provider-beta/src/main/java/com/huaweicloud/sample/ProviderController.java
+++ canary/canary-provider-beta/src/main/java/com/huaweicloud/sample/ProviderController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 /**
  * @Author GuoYl123
  * @Date 2019/10/22
@@ -15,8 +17,33 @@ public class ProviderController {
   @Value("${server.port}")
   private Integer port;
 
+  @Value("${spring.cloud.servicecomb.discovery.version}")
+  private String version;
+
   @RequestMapping("/provider")
   public String sayHello(@RequestParam("id") String id) {
-    return "beta: provider ---> " + id + " port -->" + port;
+    return "beta: provider ---> " + id + " port -->" + port + " ,version--->" + version;
+  }
+
+  @RequestMapping("/canaryFallback")
+  public String canaryFallback() {
+    return "beta: provider test fallback method, port -->" + port + " ,version--->" + version;
+  }
+
+  @RequestMapping("/canaryHeaderIsolation")
+  public String canaryHeaderIsolation(HttpServletResponse response) {
+    response.addHeader("X-HTTP-STATUS-CODE", "502");
+    return "beta: provider isolation method, port -->" + port + " ,version--->" + version;
+  }
+
+  @RequestMapping("/canaryHeaderCircuit")
+  public String canaryHeaderCircuit(HttpServletResponse response) {
+    response.addHeader("X-HTTP-STATUS-CODE", "502");
+    return "beta: provider circuit method, port -->" + port + " ,version--->" + version;
+  }
+
+  @RequestMapping("/canaryHeaderRetry")
+  public String canaryHeaderRetry() {
+    return "provider retry method, port -->" + port + " ,version--->" + version;
   }
 }
