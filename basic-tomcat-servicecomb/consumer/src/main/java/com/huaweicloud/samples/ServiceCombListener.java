c4ef365438db8e792db430633cb3b0e8a285016d
chengyouling <spadgerlin888@163.com>
2024-04-17 18:09:20 +0800

    [#132] adapter war use tomcat port register to servicecomb

--- basic-tomcat-servicecomb/consumer/src/main/java/com/huaweicloud/samples/ServiceCombListener.java
+++ basic-tomcat-servicecomb/consumer/src/main/java/com/huaweicloud/samples/ServiceCombListener.java
@@ -1,6 +1,18 @@
 package com.huaweicloud.samples;
 
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.management.Query;
+
 import com.huaweicloud.servicecomb.discovery.registry.ServiceCombAutoServiceRegistration;
+import com.huaweicloud.servicecomb.discovery.registry.ServiceCombRegistration;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.context.ConfigurableWebServerApplicationContext;
@@ -14,6 +26,9 @@ public class ServiceCombListener implements ApplicationListener<ApplicationReady
     @Autowired
     private ServiceCombAutoServiceRegistration registration;
 
+    @Autowired
+    private ServiceCombRegistration serviceCombRegistration;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         ApplicationContext context = event.getApplicationContext();
@@ -22,6 +37,43 @@ public class ServiceCombListener implements ApplicationListener<ApplicationReady
                 return;
             }
         }
+
+        // 如何需要注册的实例端口为tomcat端口，将实例信息中的endpoint端口进行调整
+//        buildInstanceWithTomcatPort();
+
         registration.start();
+    }
+
+    private void buildInstanceWithTomcatPort() {
+        List<String> endPoints = new ArrayList<>();
+        String endPoint = "";
+        if (!serviceCombRegistration.getMicroserviceInstance().getEndpoints().isEmpty()) {
+            endPoint = serviceCombRegistration.getMicroserviceInstance().getEndpoints().get(0);
+        }
+        if (StringUtils.isEmpty(endPoint)) {
+            return;
+        }
+        if (endPoint.contains("sslEnabled")) {
+            endPoints.add(endPoint.substring(0, endPoint.lastIndexOf(':') + 1) + getTomcatPort()
+                + endPoint.substring(endPoint.indexOf("?sslEnabled=")));
+        } else {
+            endPoints.add(endPoint.substring(0, endPoint.lastIndexOf(':') + 1) + getTomcatPort());
+        }
+        serviceCombRegistration.getMicroserviceInstance().setEndpoints(endPoints);
+    }
+
+    /**
+     *   获取外部tomcat端口
+     */
+    public String getTomcatPort() {
+        try {
+            MBeanServer beanServer = ManagementFactory.getPlatformMBeanServer();
+            Set<ObjectName> objectNames = beanServer.queryNames(new ObjectName("*:type=Connector,*"),
+                Query.match(Query.attr("protocol"), Query.value("HTTP/1.1")));
+            return objectNames.iterator().next().getKeyProperty("port");
+        } catch (Exception e) {
+            System.out.println("get port exception-------------------");
+            return "";
+        }
     }
 }
