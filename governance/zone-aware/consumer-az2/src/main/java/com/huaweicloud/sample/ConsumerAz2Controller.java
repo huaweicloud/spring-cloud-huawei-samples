f21da046e183b9872490845932c70940d2e66c77
chengyouling <spadgerlin888@163.com>
2026-05-30 17:22:10 +0800

    [#136] add zone-aware demo

---
+++ governance/zone-aware/consumer-az2/src/main/java/com/huaweicloud/sample/ConsumerAz2Controller.java
@@ -0,0 +1,68 @@
+package com.huaweicloud.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author chengyouling
+ * @Date 2026/5/18
+ **/
+@RestController
+public class ConsumerAz2Controller {
+  @Autowired
+  private RestTemplate restTemplate;
+
+  private final String az2Info = "provider az2, region: region2; zone: az2";
+
+  /**
+   * 同时启动az1、az2的provider服务，开启亲和，返回az2的provider信息
+   *
+   * @return 是否符合结果
+   */
+  @RequestMapping("/zone/both2Az")
+  public boolean zoneAwareBoth2Az() {
+    String result = restTemplate.getForObject("http://provider-zone/zone/both2Az", String.class);
+    return az2Info.equals(result);
+  }
+
+  /**
+   * 当仅启动az1的provider且未开启强制亲和时，返回az1的provider信息
+   *
+   * @return 是否符合结果
+   */
+  @RequestMapping("/zone/singleAz")
+  public boolean zoneAwareSingleAz() {
+    String result = restTemplate.getForObject("http://provider-zone/zone/singleAz", String.class);
+    String az1Info = "provider az1, region: region1; zone: az1";
+    return az1Info.equals(result);
+  }
+
+  /**
+   * 同时启动az1、az2的provider服务，开启强制亲和(打开application.yaml中的denyCrossZoneLoadBalancing配置后重启服务)，返回az2的provider信息
+   *
+   * @return 是否符合结果
+   */
+  @RequestMapping("/zone/denyCross/bothAz")
+  public boolean zoneAwareDenyCrossWithProviderAz2() {
+    String result = restTemplate.getForObject("http://provider-zone/zone/denyCross", String.class);
+    return az2Info.equals(result);
+  }
+
+  /**
+   * 当仅启动az1的provider且开启强制亲和时(打开application.yaml中的denyCrossZoneLoadBalancing配置后重启服务)，返回错误信息
+   *
+   * @return 是否符合结果
+   */
+  @RequestMapping("/zone/denyCross")
+  public boolean zoneAwareDenyCrossNoProviderAz2() {
+    String errorMessage = "";
+    try {
+      restTemplate.getForObject("http://provider-zone/zone/denyCross", String.class);
+    } catch (Exception e) {
+      errorMessage = e.getMessage();
+    }
+    return errorMessage.contains("No instances available");
+  }
+}
